b76de194921106e1de19da9f19961f5ee2312dc3
Ryan Dimsey <ryan@omnitouch.com.au>
2023-11-27 16:11:35 +1100

    Added asn1 codec for sbcap, compiles fine, not hooked up yet

---
+++ lib/sbcap/conv.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 by Ryan Dimsey <ryan@omnitouch.com.au>
+ *
+ * This file is part of Open5GS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if !defined(OGS_SBCAP_INSIDE) && !defined(OGS_SBCAP_COMPILATION)
+#error "This header cannot be included directly."
+#endif
+
+#ifndef OGS_SBCAP_CONV_H
+#define OGS_SBCAP_CONV_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// void ogs_s1ap_buffer_to_OCTET_STRING(
+//         void *buf, int size, SBCAP_TBCD_STRING_t *tbcd_string);
+
+// void ogs_s1ap_uint32_to_ENB_ID(
+//         SBCAP_ENB_ID_PR present, uint32_t enb_id, SBCAP_ENB_ID_t *eNB_ID);
+// void ogs_s1ap_ENB_ID_to_uint32(
+//         SBCAP_ENB_ID_t *eNB_ID, uint32_t *uint32);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OGS_SBCAP_CONV_H */
+
