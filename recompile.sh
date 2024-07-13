8a531e582e89ef4515d14d6a8c1c409dd45b6c34
Ryan Dimsey <ryan@omnitouch.com.au>
2024-07-13 13:54:16 +1000

    prod-fires: Recompile script stores executables

--- recompile.sh
+++ recompile.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 meson build --prefix=`pwd`/install
 ninja -C build install
+
+# After building we store a copy of these executables so that when we get a
+# coredump we can use it after future recompiles
+exe_folder="/var/log/open5gs/exe_$(date +%Y_%m_%d_%H%M%S)"
+mkdir $exe_folder
+cp /usr/src/open5gs/build/src/mme/open5gs-mmed $exe_folder
+cp /usr/src/open5gs/build/src/sgwc/open5gs-sgwcd $exe_folder
+cp /usr/src/open5gs/build/src/sgwu/open5gs-sgwud $exe_folder
+cp /usr/src/open5gs/build/src/smf/open5gs-smfd $exe_folder
+cp /usr/src/open5gs/build/src/upf/open5gs-upfd $exe_folder
+
+# Ensure we can get core dumps
+ulimit -c unlimited
+
 systemctl restart 'open5gs*'
